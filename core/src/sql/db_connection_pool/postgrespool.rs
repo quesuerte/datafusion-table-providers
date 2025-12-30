889961fb5ce934f35995fb956439ad94d7c83f46
Carson Blinn <blinnc@uw.edu>
2025-12-30 13:15:34 -0500

    Fixing some changes in Rust syntax

--- core/src/sql/db_connection_pool/postgrespool.rs
+++ core/src/sql/db_connection_pool/postgrespool.rs
@@ -306,11 +306,10 @@ async fn test_postgres_connection(
 async fn verify_postgres_config(config: &Config) -> Result<()> {
     for host in config.get_hosts() {
         for port in config.get_ports() {
-            if let Host::Tcp(host) = host {
-                verify_ns_lookup_and_tcp_connect(host, *port)
-                    .await
-                    .context(InvalidHostOrPortSnafu { host, port: *port })?;
-            }
+            let Host::Tcp(host) = host;
+            verify_ns_lookup_and_tcp_connect(host, *port)
+                .await
+                .context(InvalidHostOrPortSnafu { host, port: *port })?;
         }
     }
 
